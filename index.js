8bc786ab3c46725e69d86496bf672fb8ccc96a21
Donat Pllana <donat.pllana@outlook.com>
2020-02-19 19:59:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[key]=value;
  return object;
 }
-function  deleteFromObjectByKey(object, key){
-           delete object.key;
-           return object;
 }
 function deleteFromObjectByKey(object, key)
 {
